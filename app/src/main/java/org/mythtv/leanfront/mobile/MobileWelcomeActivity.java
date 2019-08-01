311f42b3e6c157c834052e66614913f5f2fa8754
Peter Bennett <pbennett@mythtv.org>
2019-08-01 11:59:47 -0400

    Refactor application package name
    Package name changed from com.example.android.tvleanback to org.mythtv.leanfront

---
+++ app/src/main/java/org/mythtv/leanfront/mobile/MobileWelcomeActivity.java
@@ -0,0 +1,15 @@
+package org.mythtv.leanfront.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.mythtv.leanfront.R;
+
+public class MobileWelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_welcome);
+    }
+}

vanderhoornm/android-MythTV-Leanfront

commit 8d19e618dc92ea7ced2c05389836fd34fe5eb84d

Fix bug: Not showing settings if offline, introduced in 9ab5f6a

app/src/main/java/org/mythtv/leanfront/ui/MainFragment.java
@@ -250,7 +250,7 @@ public class MainFragment extends BrowseSupportFragment
         if (mWasInBackground)
             restartMythTask();
         // If it's been more than an hour, refresh
-        else if (mFetchTime < System.currentTimeMillis() - 60*60*1000)
+        if (mFetchTime < System.currentTimeMillis() - 60*60*1000)
             startFetch();
         else if (mLastLoadTime < mLoadNeededTime)
             startLoader();

commit 926b793fcce3f9fe6b20de9ef5118c0959cdf4f6

Fix bug: If offline on startup not showing list when comes online, introduced in 8d19e61

app/src/main/java/org/mythtv/leanfront/data/FetchVideoService.java
@@ -87,6 +87,7 @@ public class FetchVideoService extends IntentService {
                 // Repeat if another fetch request came in while we were fetching
             }  while(MainFragment.mFetchTime > fetchTime);
         } catch (IOException | XmlPullParserException e) {
+            MainFragment.mFetchTime = 0;
             Log.e(TAG, "Error occurred in downloading videos");
             e.printStackTrace();
         }

app/src/main/java/org/mythtv/leanfront/ui/MainFragment.java
@@ -155,7 +155,7 @@ public class MainFragment extends BrowseSupportFragment
     private static MythTask mythTask = new MythTask();
     private long mLastLoadTime = 0;
     public static long mLoadNeededTime = System.currentTimeMillis();
-    public static long mFetchTime = 0;
+    public static volatile long mFetchTime = 0;
     // Keep track of the fragment currently showing, if any.
     private static MainFragment mActiveFragment = null;
     private static boolean mWasInBackground = true;
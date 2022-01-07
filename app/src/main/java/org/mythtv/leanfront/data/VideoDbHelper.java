9981badf72cc5a2a6e59aec0e3919c2d0baeb2ea
Peter Bennett <pbennett@mythtv.org>
2022-01-07 11:07:38 -0500

    synchronize VideoDbHelper creation
    Avoid the unlikely but possible case of two instances being created if
    the first two db accesses happen at the same time.

--- app/src/main/java/org/mythtv/leanfront/data/VideoDbHelper.java
+++ app/src/main/java/org/mythtv/leanfront/data/VideoDbHelper.java
@@ -48,7 +48,7 @@ public class VideoDbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public static VideoDbHelper getInstance(Context context) {
+    public static synchronized VideoDbHelper getInstance(Context context) {
         // Use the application context, which will ensure that you
         // don't accidentally leak an Activity's context.
         // See this article for more information: http://bit.ly/6LRzfx

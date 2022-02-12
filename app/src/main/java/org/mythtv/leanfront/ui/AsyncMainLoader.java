a03ae9e388b4f29f21c4b0b4f21c14399bcb7f91
Peter Bennett <pbennett@mythtv.org>
2022-02-12 14:41:42 -0500

    Main screen fix and improvement
    1. Fix fot play button on recent list intermittently not working.
    2. On main list clicking an item will open details page instead of going
    to the rec group page.

--- app/src/main/java/org/mythtv/leanfront/ui/AsyncMainLoader.java
+++ app/src/main/java/org/mythtv/leanfront/ui/AsyncMainLoader.java
@@ -385,7 +385,10 @@ public class AsyncMainLoader extends AsyncTask<MainFragment, Void, ArrayList<Arr
                 if (rectype == VideoContract.VideoEntry.RECTYPE_VIDEO) {
                     category = context.getString(R.string.row_header_videos) + "\t";
                     rowType = TYPE_VIDEODIR_ALL;
-                } else if (rectype == VideoContract.VideoEntry.RECTYPE_RECORDING
+                    itemType = TYPE_VIDEO;
+                } else if (rectype == VideoContract.VideoEntry.RECTYPE_RECORDING)
+                    itemType = TYPE_EPISODE;
+                if (rectype == VideoContract.VideoEntry.RECTYPE_RECORDING
                         || rectype == VideoContract.VideoEntry.RECTYPE_CHANNEL) {
                     category = recgroup;
                     String title;
@@ -398,7 +401,6 @@ public class AsyncMainLoader extends AsyncTask<MainFragment, Void, ArrayList<Arr
                     } else {
                         currentItem = title;
                         rowType = TYPE_RECGROUP;
-                        itemType = TYPE_SERIES;
                     }
                 }
             }

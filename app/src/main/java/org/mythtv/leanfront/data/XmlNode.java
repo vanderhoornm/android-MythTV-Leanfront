vanderhoornm/android-MythTV-Leanfront

commit 23e94c5e360c036e679205d90089c84a1a54168e

Support for Playback Groups
Playback settings can be set per playback group. Some settings
that were in playback but no longer apply per group have been
moved to "Advanced". Advanced is a new name for the settings
previously known as "Tweaks".

app/src/main/java/org/mythtv/leanfront/data/XmlNode.java
@@ -34,6 +34,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -70,8 +71,8 @@ public class XmlNode {
                             + hostname);
             XmlNode response = XmlNode.fetch(urlString, null);
             String hostIp = response.getString();
-            if (hostIp == null)
-                hostIp = backendIP;
+            if (hostIp == null || hostIp.length() == 0)
+                return "";
             urlString = XmlNode.mythApiUrl(null,
                     "/Myth/GetSetting?Key=BackendStatusPort&HostName="
                             + hostname);
@@ -301,10 +302,28 @@ public class XmlNode {
 
     public static String mythApiUrl(String hostName, String params) throws IOException, XmlPullParserException {
         String ipAndPort = getIpAndPort(hostName);
+        if (ipAndPort.length() == 0)
+            return "";
         String url = "http://" + ipAndPort;
         if (params != null)
             url = url + params;
         return url;
+    }
+
+    // need to be able to run this on a null XmlNode, that is why it is static
+    public static ArrayList<String> getStringList(XmlNode listNode) {
+        ArrayList<String> ret = new ArrayList<>();
+        ret.add("Default");
+        if (listNode != null) {
+            XmlNode stringNode = listNode.getNode("String");
+            while (stringNode != null) {
+                String value = stringNode.getString();
+                if (!"Default".equals(value))
+                    ret.add(value);
+                stringNode = stringNode.getNextSibling();
+            }
+        }
+        return ret;
     }
 
 }

app/src/main/java/org/mythtv/leanfront/data/VideoContract.java
@@ -92,6 +92,7 @@ public final class VideoContract {
         public static final String COLUMN_STORAGEGROUP = "storagegroup";
         // Empty Recgroup indicates a video file instead of a recording.
         public static final String COLUMN_RECGROUP = "recgroup";
+        public static final String COLUMN_PLAYGROUP = "playgroup";
         public static final String COLUMN_SEASON = "season";
         public static final String COLUMN_EPISODE = "episode";
         // The action intent for the result.

app/src/main/java/org/mythtv/leanfront/data/VideoDbBuilder.java
@@ -60,6 +60,7 @@ public class VideoDbBuilder {
     public static final String XMLTAG_DESCRIPTION = "Description";
     public static final String XMLTAG_STORAGEGROUP = "StorageGroup";
     public static final String XMLTAG_RECGROUP = "RecGroup";
+    public static final String XMLTAG_PLAYGROUP = "PlayGroup";
     public static final String XMLTAG_RECORDEDID = "RecordedId";
     public static final String XMLTAG_RECORDID = "RecordId";
     public static final String XMLTAG_SEASON = "Season";
@@ -194,6 +195,7 @@ public class VideoDbBuilder {
             XmlNode recordingNode = null;
             int rectype = -1;
             String recGroup = null;
+            String playGroup = null;
             String storageGroup = null;
             String channel = null;
             String airdate = null;
@@ -214,6 +216,7 @@ public class VideoDbBuilder {
                 recGroup = recordingNode.getString(XMLTAG_RECGROUP);
                 if (recGroup == null || recGroup.length() == 0)
                     recGroup = "Default";
+                playGroup = recordingNode.getString(XMLTAG_PLAYGROUP);
                 storageGroup = recordingNode.getString(XMLTAG_STORAGEGROUP);
                 channel = programNode.getString(XMLTAGS_CHANNELNAME);
                 airdate = programNode.getString(XMLTAG_AIRDATE);
@@ -249,6 +252,7 @@ public class VideoDbBuilder {
                 rectype = VideoContract.VideoEntry.RECTYPE_VIDEO;
                 recordingNode = programNode;
                 recGroup = null;
+                playGroup = null;
                 storageGroup = "Videos";
                 channel = null;
                 airdate = programNode.getString(XMLTAG_RELEASEDATE);
@@ -323,7 +327,7 @@ public class VideoDbBuilder {
             String cardImageURL = null;
             String dbFileName = null;
             dbFileName = videoFileName;
-            if (phase == 0) { // Recordings
+            if (phase == 0 && baseHostUrl.length() > 0) { // Recordings
                 cardImageURL = baseHostUrl + "/Content/GetPreviewImage?Format=png&RecordedId=" + recordedid;
             }
             if (phase == 1) { // Videos
@@ -358,6 +362,7 @@ public class VideoDbBuilder {
             videoValues.put(VideoContract.VideoEntry.COLUMN_RECORDEDID, recordedid);
             videoValues.put(VideoContract.VideoEntry.COLUMN_STORAGEGROUP, storageGroup);
             videoValues.put(VideoContract.VideoEntry.COLUMN_RECGROUP, recGroup);
+            videoValues.put(VideoContract.VideoEntry.COLUMN_PLAYGROUP, playGroup);
             videoValues.put(VideoContract.VideoEntry.COLUMN_SEASON, season);
             videoValues.put(VideoContract.VideoEntry.COLUMN_EPISODE, episode);
 

app/src/main/java/org/mythtv/leanfront/data/VideoDbHelper.java
@@ -38,7 +38,7 @@ import org.mythtv.leanfront.data.VideoContract.StatusEntry;
 public class VideoDbHelper extends SQLiteOpenHelper {
 
     // Change this when you change the database schema.
-    private static final int DATABASE_VERSION = 14;
+    private static final int DATABASE_VERSION = 15;
 
     // The name of our database.
     private static final String DATABASE_NAME = "leanback.db";
@@ -54,7 +54,7 @@ public class VideoDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion < 14) {
+        if (oldVersion < DATABASE_VERSION) {
             // On any upgrade just recreate this table
             db.execSQL("DROP TABLE IF EXISTS " + VideoEntry.TABLE_NAME);
             // Create a table to hold videos.
@@ -81,6 +81,7 @@ public class VideoDbHelper extends SQLiteOpenHelper {
                     VideoEntry.COLUMN_RECORDEDID + " TEXT," +
                     VideoEntry.COLUMN_STORAGEGROUP + " TEXT," +
                     VideoEntry.COLUMN_RECGROUP + " TEXT," +
+                    VideoEntry.COLUMN_PLAYGROUP + " TEXT," +
                     VideoEntry.COLUMN_SEASON + " TEXT," +
                     VideoEntry.COLUMN_EPISODE + " TEXT," +
                     VideoEntry.COLUMN_PROGFLAGS + " TEXT," +
@@ -113,7 +114,7 @@ public class VideoDbHelper extends SQLiteOpenHelper {
         }
 
         // View for keeping track of recently watched
-        if (oldVersion < 14) {
+        if (oldVersion < DATABASE_VERSION) {
             final String DROP_VIEW = "DROP VIEW IF EXISTS " + VideoEntry.VIEW_NAME + ";";
             db.execSQL(DROP_VIEW);
             StringBuilder createView = new StringBuilder("CREATE VIEW " + VideoEntry.VIEW_NAME);
@@ -140,6 +141,7 @@ public class VideoDbHelper extends SQLiteOpenHelper {
                         VideoEntry.COLUMN_RECORDEDID + " ," +
                         VideoEntry.COLUMN_STORAGEGROUP + " ," +
                         VideoEntry.COLUMN_RECGROUP + " ," +
+                        VideoEntry.COLUMN_PLAYGROUP + " ," +
                         VideoEntry.COLUMN_SEASON + " ," +
                         VideoEntry.COLUMN_EPISODE + " ," +
                         VideoEntry.COLUMN_PROGFLAGS + " ," +
@@ -171,6 +173,7 @@ public class VideoDbHelper extends SQLiteOpenHelper {
                     VideoEntry.COLUMN_RECORDEDID + " ," +
                     VideoEntry.COLUMN_STORAGEGROUP + " ," +
                     VideoEntry.COLUMN_RECGROUP + " ," +
+                    VideoEntry.COLUMN_PLAYGROUP + " ," +
                     VideoEntry.COLUMN_SEASON + " ," +
                     VideoEntry.COLUMN_EPISODE + " ," +
                     VideoEntry.COLUMN_PROGFLAGS + " ," +

app/src/main/java/org/mythtv/leanfront/model/Settings.java
@@ -19,11 +19,16 @@
 
 package org.mythtv.leanfront.model;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Color;
 
+import androidx.annotation.Nullable;
 import androidx.preference.PreferenceManager;
+
+import org.mythtv.leanfront.MyApplication;
 
 public class Settings {
 
@@ -38,22 +43,48 @@ public class Settings {
         if (mSingleton != null)
             return;
         mSingleton = new Settings();
-        mSingleton.setDefaults(context);
+        mSingleton.mPrefs =  PreferenceManager.getDefaultSharedPreferences (context);
+        mSingleton.mEditor = mSingleton.mPrefs.edit();
     }
 
     public static SharedPreferences.Editor getEditor() {
         return mSingleton.mEditor;
     }
 
-    public static String getString(String key) {
-        if (mSingleton != null && mSingleton.mPrefs != null)
-            return mSingleton.mPrefs.getString(key, "");
+    // Omit the "pref_" prefix when calling this
+    // This adds prefxxxx_ to string where xxxx is the group
+    // null group or "Default" = empty string
+    // If group value not found return default value.
+    public static String getString(String key, @Nullable String group) {
+        if (key.startsWith("pref_"))
+            key = key.substring(5);
+        if (mSingleton != null && mSingleton.mPrefs != null) {
+            if (group == null || group.equals("Default"))
+                group = "";
+            String actualKey = "pref" + group + "_" + key;
+            String result = mSingleton.mPrefs.getString(actualKey, null);
+            if (result == null) {
+                actualKey = "sdef" + "_" + key;
+                Context context = MyApplication.getAppContext();
+                Resources res = context.getResources();
+                int resId = res.getIdentifier(actualKey, "string", context.getPackageName());
+                if (resId != 0)
+                    result = res.getString(resId);
+            }
+            if (result == null)
+                result = "";
+            return result;
+        }
         else
             return "";
     }
 
-    public static int getInt(String key) {
-        String str = getString(key).trim();
+    public static String getString(String key) {
+        return getString(key,null);
+    }
+
+    public static int getInt(String key, @Nullable String group) {
+        String str = getString(key, group).trim();
         if (str.length() == 0)
             return 0;
         try {
@@ -64,62 +95,22 @@ public class Settings {
         }
     }
 
-    private void setDefaults(Context context) {
-        mPrefs =  PreferenceManager.getDefaultSharedPreferences (context);
-        mEditor = mPrefs.edit();
-        String str;
-        str = mPrefs.getString("pref_http_port", "6544");
-        mEditor.putString("pref_http_port",str);
-        str = mPrefs.getString("pref_bookmark", "mythtv");
-        mEditor.putString("pref_bookmark",str);
-        str = mPrefs.getString("pref_skip_fwd", "60");
-        mEditor.putString("pref_skip_fwd",str);
-        str = mPrefs.getString("pref_skip_back", "20");
-        mEditor.putString("pref_skip_back",str);
-        str = mPrefs.getString("pref_seq", "rectime");
-        mEditor.putString("pref_seq",str);
-        str = mPrefs.getString("pref_seq_ascdesc", "asc");
-        mEditor.putString("pref_seq_ascdesc",str);
-        str = mPrefs.getString("pref_audio", "auto");
-        mEditor.putString("pref_audio",str);
-        str = mPrefs.getString("pref_arrow_jump", "false");
-        mEditor.putString("pref_arrow_jump",str);
-        str = mPrefs.getString("pref_jump", "5");
-        mEditor.putString("pref_jump",str);
-        str = mPrefs.getString("pref_livetv_duration", "60");
-        mEditor.putString("pref_livetv_duration",str);
-        if (android.os.Build.VERSION.SDK_INT >= 23)
-            str = mPrefs.getString("pref_framerate_match", "false");
-        else
-            str = "false";
-        mEditor.putString("pref_framerate_match",str);
-        str = mPrefs.getString("pref_subtitle_size", "100");
-        mEditor.putString("pref_subtitle_size",str);
-        str = mPrefs.getString("pref_error_toast", "false");
-        mEditor.putString("pref_error_toast",str);
-        str = mPrefs.getString("pref_show_recents", "true");
-        mEditor.putString("pref_show_recents",str);
-        str = mPrefs.getString("pref_recents_deleted", "false");
-        mEditor.putString("pref_recents_deleted",str);
-        str = mPrefs.getString("pref_recents_watched", "false");
-        mEditor.putString("pref_recents_watched",str);
-        str = mPrefs.getString("pref_recents_trim", "true");
-        mEditor.putString("pref_recents_trim",str);
-        str = mPrefs.getString("pref_related_deleted", "false");
-        mEditor.putString("pref_related_deleted",str);
-        str = mPrefs.getString("pref_related_watched", "false");
-        mEditor.putString("pref_related_watched",str);
-        str = mPrefs.getString("pref_recents_days", "7");
-        mEditor.putString("pref_recents_days",str);
-        str = mPrefs.getString("pref_letterbox_color", String.valueOf(Color.DKGRAY));
-        mEditor.putString("pref_letterbox_color",str);
-        str = mPrefs.getString("pref_tweak_ts_search_pkts", "2600");
-        mEditor.putString("pref_tweak_ts_search_pkts",str);
-        str = mPrefs.getString("pref_livetv_rowsize", "100");
-        mEditor.putString("pref_livetv_rowsize",str);
-        str = mPrefs.getString("pref_video_parental", "4");
-        mEditor.putString("pref_video_parental",str);
+    public static int getInt(String key) {
+        return getInt(key,null);
+    }
 
-        mEditor.apply();
+    public static void putString(String key, @Nullable String group, String value) {
+        if (key.startsWith("pref_"))
+            key = key.substring(5);
+        if (mSingleton.mEditor != null) {
+            if (group == null || group.equals("Default"))
+                group = "";
+            String actualKey = "pref" + group + "_" + key;
+            mSingleton.mEditor.putString(actualKey,value);
+        }
+    }
+
+    public static void putString(String key, String value) {
+        putString(key, null, value);
     }
 }

app/src/main/java/org/mythtv/leanfront/model/Video.java
@@ -24,7 +24,6 @@
 
 package org.mythtv.leanfront.model;
 
-import android.content.SharedPreferences;
 import android.media.MediaDescription;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -48,6 +47,7 @@ public final class Video implements Parcelable, ListItem {
     public final String channel;
     public final String recordedid;
     public String recGroup;
+    public final String playGroup;
     // type takes one of the values in MainFragment to indicate
     // a type of UI element
     public int type;
@@ -140,6 +140,7 @@ public final class Video implements Parcelable, ListItem {
             final String channel,
             final String recordedid,
             final String recGroup,
+            final String playGroup,
             final String season,
             final String episode,
             final String airdate,
@@ -168,6 +169,7 @@ public final class Video implements Parcelable, ListItem {
         this.channel = channel;
         this.recordedid = recordedid;
         this.recGroup = recGroup;
+        this.playGroup = playGroup;
         this.season = season;
         this.episode = episode;
         this.airdate = airdate;
@@ -199,6 +201,7 @@ public final class Video implements Parcelable, ListItem {
         channel = in.readString();
         recordedid = in.readString();
         recGroup = in.readString();
+        playGroup = in.readString();
         season = in.readString();
         episode = in.readString();
         airdate = in.readString();
@@ -252,6 +255,7 @@ public final class Video implements Parcelable, ListItem {
         dest.writeString(channel);
         dest.writeString(recordedid);
         dest.writeString(recGroup);
+        dest.writeString(playGroup);
         dest.writeString(season);
         dest.writeString(episode);
         dest.writeString(airdate);
@@ -369,6 +373,7 @@ public final class Video implements Parcelable, ListItem {
         private String channel;
         private String recordedid;
         private String recGroup;
+        private String playGroup;
         private String season;
         private String episode;
         private String airdate;
@@ -442,6 +447,10 @@ public final class Video implements Parcelable, ListItem {
             return this;
         }
 
+        public VideoBuilder playGroup(String playGroup) {
+            this.playGroup = playGroup;
+            return this;
+        }
 
         public VideoBuilder season(String season) {
             this.season = season;
@@ -540,7 +549,7 @@ public final class Video implements Parcelable, ListItem {
                     String.valueOf(desc.getIconUri()),
                     String.valueOf(desc.getSubtitle()),
                     "", //recordid not provided
-                    "","","","","","","","","","","0","0","","","","", 0, false
+                    "","","","","","","","","","","","0","0","","","","", 0, false
             );
         }
 
@@ -557,6 +566,7 @@ public final class Video implements Parcelable, ListItem {
                     channel,
                     recordedid,
                     recGroup,
+                    playGroup,
                      season,
                      episode,
                      airdate,

app/src/main/java/org/mythtv/leanfront/model/VideoCursorMapper.java
@@ -47,6 +47,7 @@ public final class VideoCursorMapper extends CursorMapper {
     private static int channelIndex;
     private static int recordedidIndex;
     private static int recGroupIndex;
+    private static int playGroupIndex;
     private static int seasonIndex;
     private static int episodeIndex;
     private static int airdateIndex;
@@ -78,6 +79,7 @@ public final class VideoCursorMapper extends CursorMapper {
         channelIndex = cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_CHANNEL);
         recordedidIndex = cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_RECORDEDID);
         recGroupIndex = cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_RECGROUP);
+        playGroupIndex = cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_PLAYGROUP);
         seasonIndex = cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_SEASON);
         episodeIndex = cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_EPISODE);
         airdateIndex = cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_AIRDATE);
@@ -117,6 +119,7 @@ public final class VideoCursorMapper extends CursorMapper {
         String channel = cursor.getString(channelIndex);
         String recordedid = cursor.getString(recordedidIndex);
         String recGroup = cursor.getString(recGroupIndex);
+        String playGroup = cursor.getString(playGroupIndex);
         String season = cursor.getString(seasonIndex);
         String episode = cursor.getString(episodeIndex);
         String airdate = cursor.getString(airdateIndex);
@@ -151,6 +154,7 @@ public final class VideoCursorMapper extends CursorMapper {
                 .channel(channel)
                 .recordedid(recordedid)
                 .recGroup(recGroup)
+                .playGroup(playGroup)
                 .season(season)
                 .episode(episode)
                 .airdate(airdate)

app/src/main/java/org/mythtv/leanfront/ui/EditScheduleFragment.java
@@ -171,9 +171,9 @@ public class EditScheduleFragment extends GuidedStepSupportFragment
             mRecordRule.type = "Not Recording";
 
         // Lists
-        mPlayGroupList = getStringList(mDetailsList.get(2)); // ACTION_GETPLAYGROUPLIST
-        mRecGroupList = getStringList(mDetailsList.get(3)); // ACTION_GETRECGROUPLIST
-        mRecStorageGroupList = getStringList(mDetailsList.get(4)); // ACTION_GETRECSTORAGEGROUPLIST
+        mPlayGroupList = XmlNode.getStringList(mDetailsList.get(2)); // ACTION_GETPLAYGROUPLIST
+        mRecGroupList = XmlNode.getStringList(mDetailsList.get(3)); // ACTION_GETRECGROUPLIST
+        mRecStorageGroupList = XmlNode.getStringList(mDetailsList.get(4)); // ACTION_GETRECSTORAGEGROUPLIST
 
         mInputList.put(0, getContext().getString(R.string.sched_input_any));
         XmlNode inputListNode = mDetailsList.get(5); // ACTION_GETINPUTLIST
@@ -208,23 +208,6 @@ public class EditScheduleFragment extends GuidedStepSupportFragment
             }
         }
     }
-
-
-    public static ArrayList<String> getStringList(XmlNode listNode) {
-        ArrayList<String> ret = new ArrayList<>();
-        ret.add("Default");
-        if (listNode != null) {
-            XmlNode stringNode = listNode.getNode("String");
-            while (stringNode != null) {
-                String value = stringNode.getString();
-                if (!"Default".equals(value))
-                    ret.add(value);
-                stringNode = stringNode.getNextSibling();
-            }
-        }
-        return ret;
-    }
-
 
     @Override
     public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {

commit 362514d1d3e735efedac1735dd008daf0739a5bf

Fix support for v0.28, broken since 23e94c5
v0.28 does not have BackendServerAddr setting, so we default to the
connection address.

app/src/main/java/org/mythtv/leanfront/data/XmlNode.java
@@ -71,8 +71,13 @@ public class XmlNode {
                             + hostname);
             XmlNode response = XmlNode.fetch(urlString, null);
             String hostIp = response.getString();
-            if (hostIp == null || hostIp.length() == 0)
-                return "";
+            // This is needed to support mythbackend v0.28, where there is
+            // no setting for BackendServerAddr
+            if (hostIp == null)
+                hostIp = backendIP;
+            // These are removed now. I don't know why this was here
+//            if (hostIp == null || hostIp.length() == 0)
+//                return "";
             urlString = XmlNode.mythApiUrl(null,
                     "/Myth/GetSetting?Key=BackendStatusPort&HostName="
                             + hostname);
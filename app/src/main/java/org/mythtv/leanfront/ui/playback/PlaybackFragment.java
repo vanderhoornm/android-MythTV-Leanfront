vanderhoornm/android-MythTV-Leanfront

commit 51a328bbdbb2f265e2484630f4a27cec986bed98

Clean up and check for last Play Pos support
- Clean up messy constructors of AsyncBackendCall
- When refreshing, set supportLastPlayPos flag.
s flag tells us if the backend has this support (i.e. is a sufficiently
up to date version of V32 or later).

app/src/main/java/org/mythtv/leanfront/ui/playback/PlaybackFragment.java
@@ -257,8 +257,9 @@ public class PlaybackFragment extends VideoSupportFragment
         }
         if (mRecordid >= 0) {
             // Terminate Live TV
-            new AsyncBackendCall(mVideo, mRecordid, false,
-                    null).execute(
+            AsyncBackendCall call = new AsyncBackendCall(mVideo, null);
+            call.setmValue(mRecordid);
+            call.execute(
                     Video.ACTION_STOP_RECORDING,
                     Video.ACTION_REMOVE_RECORD_RULE);
         }
@@ -289,9 +290,9 @@ public class PlaybackFragment extends VideoSupportFragment
         }
 
         posBookmark = mBookmark * (long)(frameRate * 100.0f) / 100000;
-        AsyncBackendCall call =  new AsyncBackendCall(mVideo, mBookmark, mWatched,
-                null);
+        AsyncBackendCall call =  new AsyncBackendCall(mVideo, null);
         call.setPosBookmark(posBookmark);
+        call.setBookmark(mBookmark);
         posBookmark = -1;
         call.execute(Video.ACTION_SET_BOOKMARK, action2);
         try {
@@ -1012,8 +1013,9 @@ public class PlaybackFragment extends VideoSupportFragment
 
     public void markWatched(boolean watched) {
         mWatched = watched;
-        new AsyncBackendCall(mVideo, mBookmark, mWatched,
-                null).execute(Video.ACTION_SET_WATCHED);
+        AsyncBackendCall call = new AsyncBackendCall(mVideo, null);
+        call.setWatched(mWatched);
+        call.execute(Video.ACTION_SET_WATCHED);
     }
 
     /**
@@ -1024,8 +1026,10 @@ public class PlaybackFragment extends VideoSupportFragment
         long priorFileLeng = -1;
         if (multiCheck)
             priorFileLeng = mFileLength;
-        new AsyncBackendCall(mVideo, priorFileLeng, mWatched,
-                this).execute(Video.ACTION_FILELENGTH);
+        AsyncBackendCall call = new AsyncBackendCall(mVideo,
+                this);
+        call.setmValue(priorFileLeng);
+        call.execute(Video.ACTION_FILELENGTH);
     }
 
     @Override

app/src/main/java/org/mythtv/leanfront/data/AsyncBackendCall.java
@@ -59,9 +59,12 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
     }
 
     private Video mVideo;
-    // if posBookmark is >=0 use that, else use mValue for bookmark
-    private long mValue; // Used for bookmark or recordid or file length
-    private long posBookmark = -1;
+    private long mValue; // Used for recordid or file length
+    // if posBookmark is >=0 use that, else use mBookmark for bookmark
+    private long mBookmark = -1;
+    private long mPosBookmark = -1;
+    private long mLastPlay = -1;
+    private long mPosLastPlay = -1;
     private OnBackendCallListener mBackendCallListener;
     private boolean mWatched;
     private int [] mTasks;
@@ -93,32 +96,30 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
     private static long mTimeAdjustment = 0;
     private static int mythTvVersion;
 
-    public AsyncBackendCall(Video videoA, long valueA, boolean watched,
+    public AsyncBackendCall(Video videoA,
             OnBackendCallListener backendCallListener) {
         mVideo = videoA;
-        mValue = valueA;
         mBackendCallListener = backendCallListener;
-        mWatched = watched;
     }
 
     public AsyncBackendCall(OnBackendCallListener backendCallListener) {
         mBackendCallListener = backendCallListener;
     }
 
-    public void setBookmark(long mValue) {
-        this.mValue = mValue;
+    public void setBookmark(long bookmark) {
+        this.mBookmark = bookmark;
     }
 
     public long getBookmark() {
-        return mValue;
+        return mBookmark;
     }
 
     public long getPosBookmark() {
-        return posBookmark;
+        return mPosBookmark;
     }
 
     public void setPosBookmark(long posBookmark) {
-        this.posBookmark = posBookmark;
+        this.mPosBookmark = posBookmark;
     }
 
     public long getFileLength() {
@@ -197,6 +198,11 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
         return mythTvVersion;
     }
 
+    public void setmValue(long mValue) {
+        this.mValue = mValue;
+    }
+
+
     protected Void doInBackground(Integer ... tasks) {
         mTasks = new int[tasks.length];
         MainActivity main = MainActivity.getContext();
@@ -235,8 +241,8 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
             XmlNode xmlResult = null;
             switch (task) {
                 case Video.ACTION_REFRESH:
-                    mValue = 0;
-                    posBookmark = -1;
+                    mBookmark = 0;
+                    mPosBookmark = -1;
                     try {
                         if (context == null)
                             return null;
@@ -274,7 +280,7 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                         // We expect one or zero results, never more than one.
                         if (cursor.moveToNext()) {
                             int colno = cursor.getColumnIndex(VideoContract.StatusEntry.COLUMN_BOOKMARK);
-                            mValue = cursor.getLong(colno);
+                            mBookmark = cursor.getLong(colno);
                             colno = cursor.getColumnIndex(VideoContract.StatusEntry.COLUMN_SHOW_RECENT);
                             mVideo.showRecent = cursor.getInt(colno) != 0;
                         }
@@ -284,26 +290,26 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                         XmlNode bkmrkData = null;
 
                         // If no local bookmark was found look for one on MythTV
-                        if (mValue <= 0 && ("mythtv".equals(pref) || "auto".equals(pref))) {
+                        if (mBookmark <= 0 && ("mythtv".equals(pref) || "auto".equals(pref))) {
                             if (isRecording) {
                                 urlString = XmlNode.mythApiUrl(mVideo.hostname,
                                         "/Dvr/GetSavedBookmark?OffsetType=duration&RecordedId="
                                                 + mVideo.recordedid);
                                 bkmrkData = XmlNode.safeFetch(urlString, null);
                                 try {
-                                    mValue = Long.parseLong(bkmrkData.getString());
+                                    mBookmark = Long.parseLong(bkmrkData.getString());
                                 } catch (NumberFormatException e) {
                                     e.printStackTrace();
-                                    mValue = -1;
+                                    mBookmark = -1;
                                 }
                                 // sanity check bookmark - between 0 and 24 hrs.
                                 // note -1 means a bookmark but no seek table
                                 // older version of service returns garbage value when there is
                                 // no seek table.
-                                if (mValue > 24 * 60 * 60 * 1000 || mValue < 0)
-                                    mValue = -1;
+                                if (mBookmark > 24 * 60 * 60 * 1000 || mBookmark < 0)
+                                    mBookmark = -1;
                             }
-                            if (mValue == -1 || !isRecording) {
+                            if (mBookmark == -1 || !isRecording) {
                                 // look for a position bookmark (for recording with no seek table)
                                 if (isRecording)
                                     urlString = XmlNode.mythApiUrl(mVideo.hostname,
@@ -315,10 +321,10 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                                                     + mVideo.recordedid);
                                 bkmrkData = XmlNode.safeFetch(urlString, null);
                                 try {
-                                    posBookmark = Long.parseLong(bkmrkData.getString());
+                                    mPosBookmark = Long.parseLong(bkmrkData.getString());
                                 } catch (NumberFormatException e) {
                                     e.printStackTrace();
-                                    posBookmark = -1;
+                                    mPosBookmark = -1;
                                 }
                             }
                         }
@@ -355,7 +361,7 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                             }
                         }
                     } catch(IOException | XmlPullParserException e){
-                        mValue = 0;
+                        mBookmark = 0;
                         e.printStackTrace();
                     }
                     break;
@@ -396,11 +402,11 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                     }
                     break;
                 case Video.ACTION_REMOVE_BOOKMARK:
-                    mValue = 0;
-                    posBookmark = 0;
+                    mBookmark = 0;
+                    mPosBookmark = 0;
                     // fall through
                 case Video.ACTION_SET_BOOKMARK:
-                    // when using this method, mValue and posBookmark must both be set.
+                    // when using this method, mBookmark and posBookmark must both be set.
                     try {
                         found = false;
                         String pref = Settings.getString("pref_bookmark");
@@ -409,22 +415,22 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                             if (isRecording) {
                                 urlString = XmlNode.mythApiUrl(mVideo.hostname,
                                         "/Dvr/SetSavedBookmark?OffsetType=duration&RecordedId="
-                                                + mVideo.recordedid + "&Offset=" + mValue);
+                                                + mVideo.recordedid + "&Offset=" + mBookmark);
                                 xmlResult = XmlNode.safeFetch(urlString, "POST");
                                 String result = xmlResult.getString();
                                 if ("true".equals(result))
                                     found = true;
                             }
-                            if (!found && posBookmark >= 0) {
+                            if (!found && mPosBookmark >= 0) {
                                 // store a mythtv position bookmark (in case there is no seek table)
                                 if (isRecording)
                                     urlString = XmlNode.mythApiUrl(mVideo.hostname,
                                             "/Dvr/SetSavedBookmark?RecordedId="
-                                                    + mVideo.recordedid + "&Offset=" + posBookmark);
+                                                    + mVideo.recordedid + "&Offset=" + mPosBookmark);
                                 else
                                     urlString = XmlNode.mythApiUrl(mVideo.hostname,
                                             "/Video/SetSavedBookmark?Id="
-                                                    + mVideo.recordedid + "&Offset=" + posBookmark);
+                                                    + mVideo.recordedid + "&Offset=" + mPosBookmark);
                                 xmlResult = XmlNode.safeFetch(urlString, "POST");
                                 String result = xmlResult.getString();
                                 if ("true".equals(result))
@@ -434,7 +440,7 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                         // If bookmark was updated on MythTV, reset local one to 0.
                         long localBkmark = 0;
                         if (!found) {
-                            localBkmark = mValue;
+                            localBkmark = mBookmark;
                         }
 
                         // Update local bookmark
@@ -759,6 +765,29 @@ public class AsyncBackendCall extends AsyncTask<Integer, Void, Void> {
                             }
                         }
                     }
+                    // Find if we support the LastPlayPos API's
+                    long tResult = 0;
+                    XmlNode testNode = null;
+                    try {
+                        urlString = XmlNode.mythApiUrl(null,
+                                "/Dvr/GetLastPlayPos?RecordedId=-1");
+                        testNode = XmlNode.fetch(urlString, null);
+                    } catch (Exception e) {
+                        Log.e(TAG, CLASS + " Exception in GetLastPlayPos. " + e);
+                    }
+                    if (testNode != null) {
+                        try {
+                            tResult = Long.parseLong(testNode.getString());
+                        } catch (NumberFormatException e) {
+                            e.printStackTrace();
+                            tResult = 0;
+                        }
+                    }
+                    if (tResult == -1)
+                        MainFragment.supportLastPlayPos = true;
+                    else
+                        MainFragment.supportLastPlayPos = false;
+                    Log.i(TAG, CLASS + " Last Play Position Support:" + MainFragment.supportLastPlayPos);
                     break;
                 case Video.ACTION_BACKEND_INFO_HTML:
                     InputStream is = null;

app/src/main/java/org/mythtv/leanfront/presenter/CardPresenter.java
@@ -239,7 +239,7 @@ public class CardPresenter extends Presenter {
                             case TYPE_EPISODE:
                             case TYPE_VIDEO:
                             case TYPE_SERIES:
-                                new AsyncBackendCall(mVideo, 0L, false,
+                                new AsyncBackendCall(mVideo,
                                         this).execute(Video.ACTION_REFRESH);
                                 return true;
                         }

app/src/main/java/org/mythtv/leanfront/ui/EditScheduleActivity.java
@@ -51,8 +51,7 @@ public class EditScheduleActivity extends FragmentActivity implements AsyncBacke
         int chanId = getIntent().getIntExtra(CHANID, 0);
         Date startTime = (Date) getIntent().getSerializableExtra(STARTTIME);
         mRecordId = getIntent().getIntExtra(RECORDID,0);
-        AsyncBackendCall call = new AsyncBackendCall(null, 0L, false,
-                this);
+        AsyncBackendCall call = new AsyncBackendCall(this);
         int firstCall;
         if (chanId != 0 && startTime != null) {
             firstCall = 0;

app/src/main/java/org/mythtv/leanfront/ui/MainFragment.java
@@ -174,6 +174,9 @@ public class MainFragment extends BrowseSupportFragment
     private ItemViewClickedListener mItemViewClickedListener;
     private ScrollSupport scrollSupport;
     volatile boolean isLoaderRunning;
+    // This flag will be set true during refresh if it is found that we are on a
+    // backend that supports the LastPlayPos APIs (V32 or later).
+    public static boolean supportLastPlayPos;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -608,8 +611,7 @@ public class MainFragment extends BrowseSupportFragment
 
         int [] selection = getSelection();
         // Fill in disk usage
-        new AsyncBackendCall(null, 0L, false,
-                this).execute(Video.ACTION_BACKEND_INFO);
+        new AsyncBackendCall(null, this).execute(Video.ACTION_BACKEND_INFO);
         // Every time we have to re-get the category loader, we must re-create the sidebar.
         mCategoryRowAdapter.clear();
         ListRow row;
@@ -763,7 +765,7 @@ public class MainFragment extends BrowseSupportFragment
                     startFetch(recType, null, recGroup);
                     break;
                 case TYPE_INFO:
-                    new AsyncBackendCall(null, 0L, false,
+                    new AsyncBackendCall(null,
                             MainFragment.this).execute(Video.ACTION_BACKEND_INFO_HTML);
                     break;
                 case TYPE_MANAGE:

app/src/main/java/org/mythtv/leanfront/ui/SettingsActivity.java
@@ -45,8 +45,7 @@ public class SettingsActivity extends FragmentActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AsyncBackendCall call = new AsyncBackendCall(null, 0L, false,
-                this);
+        AsyncBackendCall call = new AsyncBackendCall(this);
         call.execute(Video.ACTION_GETPLAYGROUPLIST);
     }
     @Override

app/src/main/java/org/mythtv/leanfront/ui/VideoDetailsFragment.java
@@ -135,7 +135,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
     private FullWidthDetailsOverviewSharedElementHelper mHelper;
     private final VideoCursorMapper mVideoCursorMapper = new VideoCursorMapper();
     private long mBookmark = 0;     // milliseconds
-    private long posBookmark = -1;  // position in frames
+    private long mPosBookmark = -1;  // position in frames
     private SparseArrayObjectAdapter mActionsAdapter = null;
     private DetailsOverviewRow mDetailsOverviewRow = null;
     private boolean mWatched;
@@ -164,7 +164,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
 
         if (savedInstanceState != null) {
             mBookmark = savedInstanceState.getLong("mBookmark");
-            posBookmark = savedInstanceState.getLong("posBookmark");
+            mPosBookmark = savedInstanceState.getLong("posBookmark");
         }
 
         if (mSelectedVideo != null) {
@@ -177,7 +177,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
             int progflags = Integer.parseInt(mSelectedVideo.progflags);
             mWatched = ((progflags & Video.FL_WATCHED) != 0);
             if (mSelectedVideo.rectype != VideoContract.VideoEntry.RECTYPE_CHANNEL)
-                new AsyncBackendCall(mSelectedVideo, 0, false,
+                new AsyncBackendCall(mSelectedVideo,
                         this).execute(Video.ACTION_REFRESH);
 
             // When a Related Video item is clicked.
@@ -189,7 +189,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
     public void onSaveInstanceState (Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putLong("mBookmark", mBookmark);
-        outState.putLong("posBookmark", posBookmark);
+        outState.putLong("posBookmark", mPosBookmark);
     }
 
     private void removeNotification(int notificationId) {
@@ -311,10 +311,11 @@ public class VideoDetailsFragment extends DetailsSupportFragment
         if (mSelectedVideo.rectype == VideoContract.VideoEntry.RECTYPE_CHANNEL
             && (id == Video.ACTION_PLAY_FROM_BOOKMARK || id == Video.ACTION_PLAY))
             id = Video.ACTION_LIVETV;
+        AsyncBackendCall call;
         switch (id) {
             case Video.ACTION_PLAY_FROM_BOOKMARK:
                 bookmark = mBookmark;
-                posbookmark = posBookmark;
+                posbookmark = mPosBookmark;
             case Video.ACTION_PLAY:
                 Intent intent = new Intent(getActivity(), PlaybackActivity.class);
                 intent.putExtra(VideoDetailsActivity.VIDEO, mSelectedVideo);
@@ -324,42 +325,43 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                 break;
             case Video.ACTION_LIVETV:
                 setProgressBar(true);
-                new AsyncBackendCall(mSelectedVideo, 0L, false,
+                new AsyncBackendCall(mSelectedVideo,
                         this).execute(Video.ACTION_LIVETV);
                 break;
             case Video.ACTION_DELETE:
-                new AsyncBackendCall(mSelectedVideo, 0, false,
+                new AsyncBackendCall(mSelectedVideo,
                         this)
                         .execute(Video.ACTION_REFRESH, Video.ACTION_DELETE, Video.ACTION_REFRESH);
                 break;
             case Video.ACTION_DELETE_AND_RERECORD:
-                new AsyncBackendCall(mSelectedVideo, 0, false,
+                new AsyncBackendCall(mSelectedVideo,
                         this)
                         .execute(Video.ACTION_REFRESH, Video.ACTION_DELETE_AND_RERECORD, Video.ACTION_REFRESH);
                 break;
             case Video.ACTION_ALLOW_RERECORD:
-                new AsyncBackendCall(mSelectedVideo, 0, false,
+                new AsyncBackendCall(mSelectedVideo,
                         this)
                         .execute(Video.ACTION_ALLOW_RERECORD);
                 break;
             case Video.ACTION_UNDELETE:
-                new AsyncBackendCall(mSelectedVideo, 0, false,
+                new AsyncBackendCall(mSelectedVideo,
                         this)
                         .execute(Video.ACTION_UNDELETE, Video.ACTION_REFRESH);
                 break;
             case Video.ACTION_SET_WATCHED:
             case Video.ACTION_SET_UNWATCHED:
                 mWatched = (id == Video.ACTION_SET_WATCHED);
-                new AsyncBackendCall(mSelectedVideo, 0, mWatched,
-                        this)
-                        .execute(Video.ACTION_SET_WATCHED, Video.ACTION_REFRESH);
+                call = new AsyncBackendCall(mSelectedVideo, this);
+                call.setWatched(mWatched);
+                call.execute(Video.ACTION_SET_WATCHED, Video.ACTION_REFRESH);
                 break;
             case Video.ACTION_REMOVE_BOOKMARK:
                 mBookmark = 0;
-                posBookmark = 0;
-                AsyncBackendCall call = new AsyncBackendCall(mSelectedVideo, mBookmark, false,
+                mPosBookmark = 0;
+                call = new AsyncBackendCall(mSelectedVideo,
                         this);
-                call.setPosBookmark(posBookmark);
+                call.setBookmark(mBookmark);
+                call.setPosBookmark(mPosBookmark);
                 call.execute(Video.ACTION_SET_BOOKMARK, Video.ACTION_REFRESH);
                 break;
             case Video.ACTION_QUERY_STOP_RECORDING:
@@ -376,7 +378,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                 if (mSelectedVideo.recordedid != null) {
                     // Terminate a recording that may be a scheduled event
                     // so don't remove the record rule.
-                    new AsyncBackendCall(mSelectedVideo, 0, false,
+                    new AsyncBackendCall(mSelectedVideo,
                             this)
                             .execute(Video.ACTION_STOP_RECORDING,
                                     Video.ACTION_REFRESH);
@@ -394,7 +396,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                 builder.show();
                 break;
             case Video.ACTION_REMOVE_RECENT:
-                new AsyncBackendCall(mSelectedVideo, 0, false,
+                new AsyncBackendCall(mSelectedVideo,
                         this)
                         .execute(Video.ACTION_REMOVE_RECENT, Video.ACTION_REFRESH);
                 break;
@@ -425,7 +427,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                     actions.add(new Action(Video.ACTION_SET_WATCHED));
                 }
 
-                if (mBookmark > 0 || posBookmark > 0) {
+                if (mBookmark > 0 || mPosBookmark > 0) {
                     prompts.add(getString(R.string.menu_remove_bookmark));
                     actions.add(new Action(Video.ACTION_REMOVE_BOOKMARK));
                 }
@@ -491,7 +493,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                                   Intent intent) {
         if (requestCode == Video.ACTION_PLAY
                 && mSelectedVideo.rectype != VideoContract.VideoEntry.RECTYPE_CHANNEL)
-            new AsyncBackendCall(mSelectedVideo, 0, false,
+            new AsyncBackendCall(mSelectedVideo,
                     this).execute(Video.ACTION_REFRESH);
     }
 
@@ -642,7 +644,7 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                     setupDetailsOverviewRow();
                     setupMovieListRow();
                     updateBackground(mSelectedVideo.bgImageUrl);
-                    new AsyncBackendCall(mSelectedVideo, 0, false,
+                    new AsyncBackendCall(mSelectedVideo,
                             this).execute(Video.ACTION_REFRESH);
                     // When a Related Video item is clicked.
                     setOnItemViewClickedListener(itemViewClickedListener);
@@ -862,8 +864,9 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                             .build();
                     if (recordId >= 0) {
                         // Terminate Live TV
-                        new AsyncBackendCall(video, recordId, false,
-                                null).execute(
+                        AsyncBackendCall call = new AsyncBackendCall(video,  null);
+                        call.setmValue(recordId);
+                        call.execute(
                                 Video.ACTION_STOP_RECORDING,
                                 Video.ACTION_REMOVE_RECORD_RULE);
                     }
@@ -891,17 +894,18 @@ public class VideoDetailsFragment extends DetailsSupportFragment
                 break;
 
             default:
+                // Assume ACTION_REFRESH was in the list
                 if (context == null)
                     break;
                 xml = taskRunner.getXmlResult();
                 mBookmark = taskRunner.getBookmark();
-                posBookmark = taskRunner.getPosBookmark();
+                mPosBookmark = taskRunner.getPosBookmark();
                 int progflags = Integer.parseInt(mSelectedVideo.progflags);
                 mWatched = ((progflags & Video.FL_WATCHED) != 0);
                 mDetailsDescriptionPresenter.setupDescription();
                 int i = 0;
                 mActionsAdapter.clear();
-                if (mBookmark > 0 || posBookmark > 0)
+                if (mBookmark > 0 || mPosBookmark > 0)
                     mActionsAdapter.set(++i, new Action(Video.ACTION_PLAY_FROM_BOOKMARK, getResources()
                             .getString(R.string.resume_1),
                             getResources().getString(R.string.resume_2)));

commit d77805306584299af2b996bfd335a5946119ed8c

Fix Playback not setting "watched" at end
This was a bug introduced in 51a328b Clean up and check for last Play Pos support

app/src/main/java/org/mythtv/leanfront/ui/playback/PlaybackFragment.java
@@ -295,6 +295,7 @@ public class PlaybackFragment extends VideoSupportFragment
                 if (leng > 1000 && pos > leng - 10000) {
                     mWatched = true;
                     action2 = Video.ACTION_SET_WATCHED;
+                    call.setWatched(mWatched);
                 }
                 call.setLastPlay(mBookmark);
                 call.setPosLastPlay(posBookmark);
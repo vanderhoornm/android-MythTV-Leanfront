6b9418ac01057312a8963633bdc0e60d4b5e566f
Peter Bennett <pbennett@mythtv.org>
2023-12-07 15:41:29 -0500

    Revert "Hide "Manage Recordings" when there are no channels"
    This caused "manage recordings" to be hidden when in recording groups, so
    it is not useful.
    
    This reverts commit c3af0bbd7415991fd0e80494dd5db1236480d41a.

--- app/src/main/java/org/mythtv/leanfront/ui/AsyncMainLoader.java
+++ app/src/main/java/org/mythtv/leanfront/ui/AsyncMainLoader.java
@@ -99,8 +99,6 @@ public class AsyncMainLoader implements Runnable {
     private static final String TAG = "lfe";
     private static final String CLASS = "AsyncMainLoader";
 
-    boolean haveChannels = false;
-
     public AsyncMainLoader(@NonNull Activity activity) {
         this.activity = activity;
     }
@@ -544,7 +542,6 @@ public class AsyncMainLoader implements Runnable {
                             .progflags("0")
                             .build();
                     tVideo.type = TYPE_CHANNEL_ALL;
-                    haveChannels = true;
                 }
                 rowList.add(tVideo);
             }
